e0f1fbe379a295bd35f3d4f529749c96744ce6ae
Laendasill <panador@gmail.com>
2018-11-24 17:25:40 +0100

    Result class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,8 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
-    code = params[:code]
-    if code
-      @user = OauthService.call(code_params)
-      redirect_to root_path, alert: :Unauthorized unless @user[:success] == true
-    end
-  rescue RestClient::Unauthorized
-    redirect_to root_path, alert: :Unauthorized
-  end
-
-private
-
-  def code_params
-    params.require(:code)
+    @user = OauthService.call(params[:code])
+    redirect_to root_path, alert: :Unauthorized unless @user
   end
 end
--- app/services/oauth_service.rb
+++ app/services/oauth_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'pry-remote'
 
 def kill_chain(result)
@@ -5,70 +7,80 @@ def kill_chain(result)
 end
 
 def should_kill?(result)
-  return result[:success] == false
+  result.success? == false
 end
 
-class OauthService
+class Result
+  attr_accessor :value, :success
+  def initialize(val, success = true)
+    @value = val
+    @success = success
+  end
 
+  def success?
+    @success
+  end
+
+  def map
+    if success?
+      begin
+      Result.new(yield(@value))
+    rescue Exception => e
+      Result.error(e)
+    end
+    else
+      self
+    end
+  end
+
+  def self.error(error)
+    new(error.message, false)
+  end
+end
+class OauthService
   def self.call(*args)
     new(args).call
   end
-  
+  @@CallOrder = %w[get_token parse_token get_user_info parse_user_info]
   def initialize(code)
-      @params = {
-        grant_type: :authorization_code,
-        client_id: Rails.application.credentials[:UID],
-        client_secret: Rails.application.credentials[:secret],
-        redirect_uri: Rails.application.credentials[:callback_uri],
-        code: code
-      }
+    @logger = Logger.new(STDOUT)
+
+    @params = {
+      grant_type: :authorization_code,
+      client_id: Rails.application.credentials[:UID],
+      client_secret: Rails.application.credentials[:secret],
+      redirect_uri: Rails.application.credentials[:callback_uri],
+      code: code[0]
+    }
   end
 
   def call
-    result = ['get_token','parse_token','get_user_info','parse_user_info']
-      .inject({success:true, value:@params}) { |sum,el|
-        return send(el, sum)
-      }
-    result
+    result = @@CallOrder.inject(Result.new(@params)) do |sum, el|
+      send(el, sum)
+    end
+    return result.value unless result.success? == false
+
+    false
   end
 
   def get_token(result)
-    return kill_chain(result) if should_kill?(result)
-    response = RestClient.post('https://shielded-temple-86762.herokuapp.com/oauth/token', result[:value])
-
-    {success: true, value: response}
-  rescue Exception => e
-
-    {success: false, value: e.message}
+    @logger.debug('in get_token')
+    result.map do |value|
+      RestClient.post('https://shielded-temple-86762.herokuapp.com/oauth/token', value)
+    end
   end
 
   def parse_token(result)
-
-    return kill_chain(result) if should_kill?(result)
-    access_token = JSON.parse(result[:value])['access_token']
-
-    {success: true, result: access_token}
-  rescue Exception => e
-    {success: false, value: e.message}
+    result.map { |val| JSON.parse(val)['access_token'] }
   end
 
   def get_user_info(result)
-    return kill_chain(result) if should_kill?(result)
-
-    response = RestClient.get('https://shielded-temple-86762.herokuapp.com/api/user/?token=' + access_token)
-    
-    {success: true, value: response}
-  rescue Exception => e
-    {success: false, value: e.message}
+    result.map do |value|
+      RestClient.get('https://shielded-temple-86762.herokuapp.com/api/user/?token=' + value)
+    end
   end
 
   def parse_user_info(result)
-    return kill_chain(result) if should_kill?(result)
-    
-    user = JSON.parse(response)
-    {success: true, value: user}
-  rescue  Exception => e
-    {success: false, value: e.message}
+    result.map { |val| JSON.parse(val) }
   end
-
 end
